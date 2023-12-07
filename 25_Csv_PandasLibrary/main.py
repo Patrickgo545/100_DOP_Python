a89088f0536975986e086becde57457031028502
Patrick Go <patrickgo545@gmail.com>
2023-12-07 15:18:58 -0500

    Day 25 - Progress

--- 25_Csv_PandasLibrary/main.py
+++ 25_Csv_PandasLibrary/main.py
@@ -45,3 +45,29 @@ average_temp = sum(temp_list) / len(temp_list)
 
 average_temp_function = data['temp'].mean()
 # print(average_temp_function)
+
+highest_temp = data['temp'].max()
+# print(highest_temp)
+
+# GET DATA IN COLUMNS   
+# print(data['condition'])
+# print(data.condition)
+
+# PRINT A ROW
+# print(data[data.day == 'Monday'])
+
+# CHALLENGE - print row with the highest tem
+# print(data[data.temp == data.temp.max()])
+
+monday = data[data.day == 'Monday']
+# print(monday.condition)
+# print((monday.temp * 9/5) + 32)
+
+# CREATE A DATAFRAME FROM SCRATCH
+dict_data = {
+    "students": ["Amy", "James", "Angela"],
+    "scores": [76, 57, 65]
+}
+
+new_dataframe = pd.DataFrame(dict_data)
+new_dataframe.to_csv("new_data.csv")
